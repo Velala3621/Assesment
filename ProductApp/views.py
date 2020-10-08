a20ec6032a90355f47f1f051c40d185ef0dd67c4
chaitanya <pavanchaitanya9@gmail.com>
2020-10-08 16:07:05 +0530

    second commit

--- ProductApp/views.py
+++ ProductApp/views.py
@@ -6,22 +6,22 @@ from .models import Product
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.utils.decorators import method_decorator
 
-@method_decorator(login_required, name='dispatch')
+
 class productlist(ListView):
     model = Product
 
-@method_decorator(login_required, name='dispatch')
+#@method_decorator(login_required, name='dispatch')
 class ProfileCreate(CreateView):
     model = Product
     fields = ['title', 'description','image']
 
 
-@method_decorator(login_required, name='dispatch')
+#@method_decorator(login_required, name='dispatch')
 class ProductItemCreate(CreateView):
     model = Product
     fields = ['title', 'description','image']
@@ -87,21 +87,30 @@ def UserRegistration(request):
     form=CreateUserForm()
     if request.method == "POST":
         form = CreateUserForm(request.POST)
-        user=form.save()
-        user.set_password(user.password)
-        user.save()
-        return redirect("login")
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            return redirect("login")
+        else:
+            return render(request, 'registration.html', {'form': form})
+
     else:
         return render(request,'registration.html',{'form':form})
 
 def loginview(request):
     if request.method == "POST":
-        useR=request.POST.get("username")
-        passworD=request.POST.get("password")
-        x=authenticate(request,username=useR,password=passworD)
-        if x is not None:
-            login(request,x)
-            return redirect("product-add")
+        user1=request.POST['username']
+        password1=request.POST['password']
+        print(user1)
+        print(password1)
+        user = authenticate(username=user1, password=password1)
+        if user is not None:
+            print("in authentication")
+            login(request,user)
+            return HttpResponseRedirect("/product-add")
         else:
             messages.info(request, "username or password is invalid")
     return render(request, 'registration/login.html')
